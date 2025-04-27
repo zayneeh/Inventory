b82533866d714ae789e56a3b43c63c1ae7b1f7ad
Zayneeh <tairuzainab5@gmail.com>
2025-04-27 18:56:17 +0100

    Work flow

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from src import get_data_from_pdf, align_content, extract_data
+from scripts.Extract_data import get_data_from_pdf
+from scripts.Load_in_json import extract_data
+from scripts.Align import align_data
+import json
 
 def main():
-    pdf_path = "data/sample.pdf"
-    output_path = "output/result.json"
+    pdf_path = "data\home_inventory.pdf"
+    output_path = r"data\result.json"
 
     raw_text = get_data_from_pdf(pdf_path)
-    aligned_content = align_content(raw_text)
-    extracted_data = extract_data(aligned_content)
+    aligned_content = align_data(raw_text)
+    extracted_data = extract_data(raw_text, aligned_content)
+
+    #Save to JSON file
+    with open(output_path, "w") as f:
+        json.dump(extracted_data, f)
 
     print(f"Data saved to {output_path}")
 
