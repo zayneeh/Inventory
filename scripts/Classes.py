2f98aab0f5dd5803e2f1453ff6613b40b118abe1
Zayneeh <tairuzainab5@gmail.com>
2025-04-28 03:13:52 +0100

    added items source, stle and area as individual columns

--- scripts/Classes.py
+++ scripts/Classes.py
@@ -1,13 +1,15 @@
 class OwnerInfo:
-    def __init__(self, owner_name="", owner_address="", owner_telephone=""):
+    def __init__(self, owner_name, owner_address, owner_telephone):
         self.owner_name = owner_name
         self.owner_address = owner_address
         self.owner_telephone = owner_telephone
 
 class Inventory:
-    def __init__(self, purchase_date="", serial_number="", description="", source_style_area="", value=""):
+    def __init__(self, purchase_date, serial_number, description, source, style, area, value):
         self.purchase_date = purchase_date
         self.serial_number = serial_number
         self.description = description
-        self.source_style_area = source_style_area
+        self.source = source
+        self.style = style
+        self.area = area
         self.value = value
